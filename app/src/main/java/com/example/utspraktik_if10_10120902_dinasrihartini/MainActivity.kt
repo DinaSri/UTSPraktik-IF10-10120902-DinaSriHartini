3ff8f44d040bfd602985aa8e89841b0074b32cda
Dina Sri Hartini <dinahartini01@gmail.com>
2023-05-30 21:10:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if10_10120902_dinasrihartini/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.utspraktik_if10_10120902_dinasrihartini
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Nama    : Dina Sri Hartini
+ * NIM     : 10120902
+ * Kelas   : IF-10
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.uts10120902)
+    }
+}
